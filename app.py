9a3916d29b81b5163bdc2f57f05572ce436b5223
Mukku <mukeshkmr7544@gmail.com>
2023-04-04 01:27:09 +0530

    test commit

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-print("hello")
+'''print("hello")
 print("Mukesh")
 print("Kumar")
 print("Data scientist")
-print("Machine Learning Engineer")
+print("Machine Learning Engineer")'''
+num = int (input("enter 3 digit number"))
+a = num%10
+b = num//10
+c = b%10
+d = b//10
+list2 = [d,b,a]
+list2
+print(list1)
