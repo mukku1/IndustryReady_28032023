d48a3f169920f00a8f260e649ac18bbb17a9a54b
Mukku <mukeshkmr7544@gmail.com>
2023-04-05 01:55:31 +0530

    practice set Test 1 completed

---
+++ if_else_practice_questions.py
@@ -0,0 +1,19 @@
+## 70+ python if else statement important practice questions
+
+# 1. write a program to check whether person is eligible for vote or not 
+age = int(input("enter age"))
+
+if age>=18:
+    print("eligible to vote")
+else:
+    print("not eligible to vote")
+
+
+# 2. to check number entered by user is odd or even
+
+num = int(input("enter number"))
+
+if num%2==0:
+    print("even")
+else:
+    print("odd")
