1fd6b1edd6b4849179561ce4667dffb8f6149056
Mukku <mukeshkmr7544@gmail.com>
2023-04-06 01:41:21 +0530

    practice part

--- practice.py
+++ practice.py
@@ -13,3 +13,25 @@ elif email=="mukesh@gmail.com" and password!="mukesh":
       print("incorrect password")
 else:
   print("login is incorrect")
+
+
+
+
+## menu driven calculator
+
+num1 = int(input("enter 1st number"))
+num2 = int(input("enter 2nd numbre"))
+
+op = input("enter operation")
+
+if op=="+":
+  print(num1+num2)
+elif op=="-":
+  if num1>num2:
+    print(num1-num2)
+  else:
+    print(num2-num1)
+elif op=="*":
+  print(num1*num2)
+else:
+  print((num1/num2)//10)
